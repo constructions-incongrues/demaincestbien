40debcd8f217675f52674d7f727fa185da998557
Nelly Babillon <89858702+nota2k@users.noreply.github.com>
2024-06-02 02:37:00 +0000

    Add scroll effect + pagination

--- user/themes/demaincestbien/js/demain.js
+++ user/themes/demaincestbien/js/demain.js
@@ -8,3 +8,12 @@ let newsletter = document.querySelector('.newsletter');
 closeBtn.addEventListener('click', () => {  
     newsletter.style.display = 'none';
 })
+
+// Hide last border of event items
+
+let eventItems = document.querySelectorAll('.event');
+eventItems.forEach((item, index) => {  
+    if(index === eventItems.length - 1) {
+        item.style.borderBottom = 'none';
+    }
+})
